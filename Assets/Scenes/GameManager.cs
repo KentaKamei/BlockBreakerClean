6bef07955cfeca4c42309a03f06d29ac11ed1bcd
KentaKamei <g1272040@tcu.ac.jp>
2025-03-23 16:51:40 +0900

    Add Pause

--- Assets/Scenes/GameManager.cs
+++ Assets/Scenes/GameManager.cs
@@ -6,8 +6,9 @@ using UnityEngine.SceneManagement; // リスタート用
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance; // シングルトンでどこからでもアクセスできるように
-
     private bool isGameOver = false;
+    private bool isPaused = false;
+    public GameObject pauseText;
 
     void Awake()
     {
@@ -22,6 +23,21 @@ public class GameManager : MonoBehaviour
         if (!isGameOver && GameObject.FindObjectsOfType<BlockController>().Length == 0)
         {
             GameClear();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TogglePause();
+        }
+    }
+
+    public void TogglePause()
+    {
+        isPaused = !isPaused;
+        Time.timeScale = isPaused ? 0f : 1f; 
+        if (pauseText != null)
+        {
+            pauseText.SetActive(isPaused);
         }
     }
 
@@ -44,6 +60,11 @@ public class GameManager : MonoBehaviour
         }
         // ここにゲームクリア演出やUI表示など追加できる
         SceneManager.LoadScene("GameClear");
+    }
+
+    public bool IsPaused()
+    {
+        return isPaused;
     }
 }
 
